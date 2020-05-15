23569d36cb6a48d2c6417a78deb24195077d1acd
Karoon Phatangjaijing <karoonp@gmail.com>
2020-05-15 02:00:46 -0700

    add start/stop script

--- docker_start.sh
+++ docker_start.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 
-docker start adidas-related-info_related_info_1 adidas-related-info_mysql_1 adidas-related-info_related_info_1
+docker start adidas-related-info_related_info_1 adidas-related-info_mysql_1 
--- docker_stop.sh
+++ docker_stop.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 
-docker stop adidas-related-info_related_info_1 adidas-related-info_mysql_1 adidas-related-info_related_info_1
+docker stop adidas-related-info_related_info_1 adidas-related-info_mysql_1 
